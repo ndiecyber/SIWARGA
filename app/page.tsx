a0cc841c6ee8ed2360702b432d888c4e01e15e7c
raihannurhidayat <grandrx041@gmail.com>
2026-06-08 20:06:33 +0700

    initalization(project): create repository for project siwarga

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import PublicLayout from "@/components/layouts/public-layout";
+import LandingPage from "@/features/landing/pages/landing-page";
+
+export default function Home() {
+  return (
+    <PublicLayout>
+      <LandingPage />
+    </PublicLayout>
+  );
+}
