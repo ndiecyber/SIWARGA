1bc576abb4a6a14e3476741309d3bf979d5f04ab
raihannurhidayat <grandrx041@gmail.com>
2026-06-18 14:47:51 +0700

    fix(auth): change url env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,10 @@ module.exports = {
       env: {
         NODE_ENV: "production",
         PORT: 3000,
+        // Ensure PM2 passes down your auth variables
+        BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+        BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+        DATABASE_URL: process.env.DATABASE_URL,
       },
     },
   ],
